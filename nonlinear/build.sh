42ccff23e5b125f925b188bafd58e156fb6756ae
liyp <liyp1120@yeah.net>
2022-12-22 21:55:08 +0800

    add nonlinear/build.sh, nonlinear/clean.sh

---
+++ nonlinear/build.sh
@@ -0,0 +1,4 @@
+gcc mm.c -fopenmp -o mm
+gcc ac_down.c -fopenmp -o ac_down
+gcc cp_up.c -fopenmp -lm -o cp_up
+gcc ms_non.c -fopenmp -o ms_non
---
+++ nonlinear/clean.sh
@@ -0,0 +1,4 @@
+rm mm a.out
+rm ac_down
+rm cp_up
+rm ms_non
